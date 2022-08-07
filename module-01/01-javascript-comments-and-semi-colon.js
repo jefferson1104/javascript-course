0ae7d8c0598c7c208d6bef80531f85763f9037c8
jefferson1104 <jefferson1104junior@gmail.com>
2022-08-07 16:05:51 -0300

    commnets, semi-colons, variables and data-types

---
+++ module-01/01-javascript-comments-and-semi-colon.js
@@ -0,0 +1,26 @@
+// this is a comment, Single-line comment
+
+/*
+this
+is
+a
+multi-line
+comment
+*/
+
+// The semi-colon in JavaScript
+/* 
+In the English language, the fullstop or period - the . character - is used to separate thoughts into sentences.
+
+By clearly separating thoughts with the fullstop, you avoid being misunderstood.
+
+In JavaScript, the semi-colon - the ; character - has a similar purpose: it is used to clearly delimit parts of the code from some other parts of the code.
+*/
+console.log("Hello, World");
+
+/*
+did you know that you can style the output in the console?
+
+In this code snippet, there are a few additions: the font size is different and the color is blue: 
+*/
+console.log("%cHello, World", "color: blue; font-size: 40px");
